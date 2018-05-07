8a0ac8a4cde98b8254594639a7a565f8fa8a8b7d
gunvora <gunvor_a@telia.com>
2018-05-07 10:43:00 +0200

    http -> https

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -14,7 +14,7 @@ class App extends React.Component {
   }
 
   fetchData = () => {
-    fetch("http://api.sr.se/api/v2/channels?format=json&size=100").then((response) => {
+    fetch("https://api.sr.se/api/v2/channels?format=json&size=100").then((response) => {
       return response.json()
     }).then((json) => {
       console.log("here is data:", json)
